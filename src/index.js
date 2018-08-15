6f33ab2290f733fd550a4a3fc3be30c18d7cf4c1
Tatiana Ignatenko <tignatenko@office.local>
2018-08-15 17:08:58 +0700

    Add Page component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {render} from 'react-dom';
 
 import Card from './components/Card/Card';
+import Page from './components/Page/Page';
 
 const mountNode = document.getElementById('root');
 const description = `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                      Ut enim ad minim veniam, quis nostrud exercitation ullamco.`;
-render(<Card title='Card1' description={description} editHandler={() => {}}/>, mountNode);
+const pageContent = <Card title='Card1' description={description} editHandler={() => {}}/>;
+render(<Page title='Card List' content={pageContent}/>, mountNode);
