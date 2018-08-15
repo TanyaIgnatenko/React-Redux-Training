284fb08d8ea415874b841346237eba32923341d3
Tatiana Ignatenko <tignatenko@office.local>
2018-08-15 18:45:28 +0700

    Add Grid component

---
+++ src/components/Grid/Grid.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Grid.css';
+
+export default function Grid(props) {
+    const elems = props.elems.map((elem, idx) => <div key={idx} display='inline'> {elem} </div>);
+    return (
+        <div className='grid__container'>
+            {elems}
+        </div>
+    );
+}
+
+Grid.propTypes = {
+    elems: PropTypes.arrayOf(PropTypes.element).isRequired
+};
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import {render} from 'react-dom';
 
 import Card from './components/Card/Card';
 import Page from './components/Page/Page';
+import Grid from './components/Grid/Grid';
 
-const mountNode = document.getElementById('root');
 const description = `Li Europan lingues es membres del sam familie. Lor separat existentie es un myth.
                      Por scientie, musica, sport etc, litot Europa usa li sam vocabular.
                      Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules.
                      Omnicos directe al desirabilite de un nov lingua franca.`;
-const pageContent = <Card title='Card1' description={description} editHandler={() => {}}/>;
-render(<Page title='Card List' content={pageContent}/>, mountNode);
+
+const mountNode = document.getElementById('root');
+const card = <Card title='Card1' description={description} editHandler={() => {}}/>;
+const cardList = [card, card, card, card, card, card, card, card];
+const grid = <Grid elems={cardList}/>;
+
+render(<Page title='Card List' content={grid}/>, mountNode);
