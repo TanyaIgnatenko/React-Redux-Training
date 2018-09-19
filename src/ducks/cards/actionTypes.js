b839b09ebb6175439e9456624651ff022228eb23
Tatiana Ignatenko <tignatenko@office.local>
2018-09-19 11:28:26 +0700

    Combine card action type constants of one type

--- src/ducks/cards/actionTypes.js
+++ src/ducks/cards/actionTypes.js
@@ -1,18 +1,38 @@
-const ADD_CARD = '@@cards/ADD_CARD';
-const EDIT_CARD = '@@cards/ADD_CARD';
-const REMOVE_CARD = '@@cards/REMOVE_CARD';
-const FETCH_CARDS_SUCCESS = '@@cards/FETCH_CARDS_SUCCESS';
-const FETCH_CARD_SUCCESS = '@@cards/FETCH_CARD_SUCCESS';
-const FETCH_TEMP_CARD_SUCCESS = '@@cards/FETCH_TEMP_CARD_SUCCESS';
-const DELETE_TEMP_CARDS = '@@cards/DELETE_TEMP_CARDS';
+const ADD_CARD = {
+    REQUEST: '@@cards/ADD_CARD_REQUEST',
+    SUCCESS: '@@cards/ADD_CARD_SUCCESS',
+    ERROR: '@@cards/ADD_CARD_ERROR'
+};
+
+const EDIT_CARD = {
+    REQUEST: '@@cards/EDIT_CARD_REQUEST',
+    SUCCESS: '@@cards/EDIT_CARD_SUCCESS',
+    ERROR: '@@cards/EDIT_CARD_ERROR'
+};
+
+const REMOVE_CARD = {
+    REQUEST: '@@cards/REMOVE_CARD_REQUEST',
+    SUCCESS: '@@cards/REMOVE_CARD_SUCCESS',
+    ERROR: '@@cards/REMOVE_CARD_ERROR'
+};
+
+const FETCH_CARDS = {
+    REQUEST: '@@cards/FETCH_CARDS_REQUEST',
+    SUCCESS: '@@cards/FETCH_CARDS_SUCCESS',
+    ERROR: '@@cards/FETCH_CARDS_ERROR'
+};
+
+const FETCH_CARD = {
+    REQUEST: '@@cards/FETCH_CARD_REQUEST',
+    SUCCESS: '@@cards/FETCH_CARD_SUCCESS',
+    ERROR: '@@cards/FETCH_CARD_ERROR'
+};
 
 export {
     ADD_CARD,
     EDIT_CARD,
     REMOVE_CARD,
-    FETCH_CARDS_SUCCESS,
-    FETCH_CARD_SUCCESS,
-    FETCH_TEMP_CARD_SUCCESS,
-    DELETE_TEMP_CARDS
+    FETCH_CARDS,
+    FETCH_CARD
 };
 
