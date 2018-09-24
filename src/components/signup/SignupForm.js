7a0cdf48de3f53d8fab277b1e280e608eb22c02f
Tatiana Ignatenko <tignatenko@office.local>
2018-09-24 07:17:39 +0700

    Fix login and sign up form styles

--- src/components/signup/SignupForm.js
+++ src/components/signup/SignupForm.js
@@ -1,64 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {FIELD_LABEL, FIELD_PLACEHOLDER} from '../../locale';
+import {BUTTON, FIELD_LABEL, FIELD_PLACEHOLDER} from '../../locale';
 import {Button, Col, Form, FormGroup, Input, Label} from 'reactstrap';
+import './SignupForm.scss';
 
 function SignupForm(props) {
     return (
-        <Form>
-            <FormGroup row>
-                <Label for='name' sm={2}>{FIELD_LABEL.NAME}</Label>
-                <Col sm={10}>
+        <div className={'signup-form__container centering-container'}>
+            <Form>
+                <FormGroup>
+                    <Label for='name'>{FIELD_LABEL.NAME}</Label>
                     <Input
+                        className={'signup-form__input'}
                         name='name'
                         id='name'
                         placeholder={FIELD_PLACEHOLDER.NAME}
                         onChange={props.onChange}
                     />
-                </Col>
-            </FormGroup>
-            <FormGroup row>
-                <Label for='email' sm={2}>{FIELD_LABEL.EMAIL}</Label>
-                <Col sm={10}>
+                </FormGroup>
+                <FormGroup>
+                    <Label for='email'>{FIELD_LABEL.EMAIL}</Label>
                     <Input
-                        type='email'
+                        className={'signup-form__input'}
                         name='email'
                         id='email'
                         placeholder={FIELD_PLACEHOLDER.EMAIL}
                         onChange={props.onChange}
                     />
-                </Col>
-            </FormGroup>
-            <FormGroup row>
-                <Label for='password' sm={2}>{FIELD_LABEL.PASSWORD}</Label>
-                <Col sm={10}>
+                </FormGroup>
+                <FormGroup>
+                    <Label for='password'>{FIELD_LABEL.PASSWORD}</Label>
                     <Input
-                        type='password'
+                        className={'signup-form__input'}
                         name='password'
                         id='password'
                         placeholder={FIELD_PLACEHOLDER.PASSWORD}
                         onChange={props.onChange}
                     />
-                </Col>
-            </FormGroup>
-            <FormGroup row>
-                <Label for='confirmPassword' sm={2}>{FIELD_LABEL.CONFIRM_PASSWORD}</Label>
-                <Col sm={10}>
+                </FormGroup>
+                <FormGroup>
+                    <Label for='confirmPassword'>{FIELD_LABEL.CONFIRM_PASSWORD}</Label>
                     <Input
-                        type='password'
+                        className={'signup-form__input'}
                         name='confirmPassword'
                         id='confirmPassword'
                         placeholder={FIELD_PLACEHOLDER.CONFIRM_PASSWORD}
                         onChange={props.onChange}
                     />
-                </Col>
-            </FormGroup>
-            <FormGroup>
-                <Col sm={{size: 10, offset: 2}}>
-                    <Button onClick={props.onSignupClick}>{Button.SIGN_UP}</Button>
-                </Col>
-            </FormGroup>
-        </Form>
+                </FormGroup>
+                <FormGroup className={'button-container'}>
+                    <Button className={'signup-form__button'}onClick={props.onSignupClick}>{BUTTON.SIGN_UP}</Button>
+                </FormGroup>
+            </Form>
+        </div>
     );
 }
 
