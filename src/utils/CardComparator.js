108f023d45952c44c319780ffeba29ea6a421298
Tatiana Ignatenko <tignatenko@office.local>
2018-09-24 11:04:49 +0700

    Delete unused file

--- src/utils/CardComparator.js
+++
@@ -1,9 +0,0 @@
-export default function isEqual(post1, post2) {
-    if (post1.id === post2.id &&
-        post1.title === post2.title &&
-        post1.content === post2.content &&
-        post1.likeCount === post2.likeCount) {
-        return true;
-    }
-    return false;
-}
