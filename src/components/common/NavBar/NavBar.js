b487046a626508d693e00e2fd877aa0de4cb8359
Tatiana Ignatenko <tignatenko@office.local>
2018-09-24 23:28:43 +0700

    Replace react-bootstrap navbar with reactstrap one

--- src/components/common/NavBar/NavBar.js
+++ src/components/common/NavBar/NavBar.js
@@ -1,39 +1,69 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Nav, Navbar, NavItem} from 'react-bootstrap';
+import {Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink} from 'reactstrap';
 import {Routes} from '../../../config';
 import {APP_NAME, BUTTON} from '../../../locale';
 
-function NavBar(props) {
-    const signinButton = !props.isAuth ? <NavItem onClick={props.onLogin}>{BUTTON.SIGN_IN}</NavItem> : null;
-    const signupButton = !props.isAuth ? <NavItem onClick={props.onSignup}>{BUTTON.SIGN_UP}</NavItem> : null;
-    const logoutButton = props.isAuth ? <NavItem onClick={props.onLogout}>{BUTTON.LOGOUT}</NavItem> : null;
-    return (
-        <Navbar inverse collapseOnSelect>
-            <Navbar.Header>
-                <Navbar.Brand>
-                    <a href={Routes.ROOT}>{APP_NAME}</a>
-                </Navbar.Brand>
-                <Navbar.Toggle/>
-            </Navbar.Header>
-            <Navbar.Collapse>
-                <Nav pullRight>
-                    {signinButton}
-                    {signupButton}
-                    {logoutButton}
-                </Nav>
-                <Navbar.Text pullRight>{props.username}</Navbar.Text>
-            </Navbar.Collapse>
-        </Navbar>
-    );
-}
+class NavBar extends React.Component {
+    static propTypes = {
+        isAuth: PropTypes.bool.isRequired,
+        onLogout: PropTypes.func.isRequired,
+        onLogin: PropTypes.func.isRequired,
+        onSignup: PropTypes.func.isRequired,
+        username: PropTypes.string
+    };
 
-NavBar.propTypes = {
-    isAuth: PropTypes.bool.isRequired,
-    onLogout: PropTypes.func.isRequired,
-    onLogin: PropTypes.func.isRequired,
-    onSignup: PropTypes.func.isRequired,
-    username: PropTypes.string
-};
+    state = {
+        isOpen: false
+    };
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    };
+
+    render() {
+        const {onLogin, onSignup, onLogout, username, isAuth} = this.props;
+        return (
+            <Navbar color='dark' dark expand='md'>
+                <NavbarBrand href={Routes.ROOT}>
+                    {APP_NAME}
+                </NavbarBrand>
+                <NavbarToggler onClick={this.toggle}/>
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className='ml-auto' navbar>
+                        {
+                            username &&
+                            <NavItem>
+                                <NavLink disabled>
+                                    {username}
+                                </NavLink>
+                            </NavItem>
+                        }
+                        {
+                            !isAuth &&
+                            <NavItem>
+                                <NavLink onClick={onLogin}>{BUTTON.SIGN_IN}</NavLink>
+                            </NavItem>
+                        }
+                        {
+                            !isAuth &&
+                            <NavItem>
+                                <NavLink onClick={onSignup}>{BUTTON.SIGN_UP}</NavLink>
+                            </NavItem>
+                        }
+                        {
+                            isAuth &&
+                            <NavItem>
+                                <NavLink onClick={onLogout}>{BUTTON.LOGOUT}</NavLink>
+                            </NavItem>
+                        }
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        );
+    }
+}
 
 export default NavBar;
