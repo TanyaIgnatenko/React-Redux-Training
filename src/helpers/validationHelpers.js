89940a595a9a2516a7c502dcafc62c088b8e3e17
Tatiana Ignatenko <tignatenko@office.local>
2018-09-26 15:09:01 +0700

    Fix regexp for email validation

--- src/helpers/validationHelpers.js
+++ src/helpers/validationHelpers.js
@@ -1,6 +1,6 @@
 const isValidName = name => /^[a-zA-Z]\S\S+$/.test(name);
 
-const isValidEmail = email => /^\w{3,}@(a-zA-Z)+\.(a-zA-Z)+$/.test(email);
+const isValidEmail = email => /^\w{3,}@[a-zA-Z]+\.[a-zA-Z]+$/.test(email);
 
 const isValidPassword = password => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/.test(password);
 
