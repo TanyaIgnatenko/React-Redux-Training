7fb135b08de0f8a9240dcac0d8ad82de79b9b5ac
Tatiana Ignatenko <tignatenko@office.local>
2018-09-27 15:51:07 +0700

    Fix width of login and signup reactstrap forms

--- src/components/signup/SignupForm.js
+++ src/components/signup/SignupForm.js
@@ -10,71 +10,76 @@ function SignupForm(props) {
     const {nameValid, emailValid, passwordValid, confirmPasswordValid} = props;
     const {onBlurName, onBlurEmail, onBlurPassword, onBlurConfirmPassword} = props;
     return (
-        <Container className={'signup-form__container'}>
-            <Form>
-                <FormGroup>
-                    <Label for='name'>{FIELD_LABEL.NAME}</Label>
-                    <Input
-                        className={'signup-form__input'}
-                        name='name'
-                        id='name'
-                        placeholder={FIELD_PLACEHOLDER.NAME}
-                        onChange={props.onChange}
-                        invalid={nameInvalid}
-                        valid={nameValid}
-                        onBlur={onBlurName}
-                    />
-                    <FormFeedback>{RULE.NAME}</FormFeedback>
-                </FormGroup>
-                <FormGroup>
-                    <Label for='email'>{FIELD_LABEL.EMAIL}</Label>
-                    <Input
-                        className={'signup-form__input'}
-                        name='email'
-                        id='email'
-                        placeholder={FIELD_PLACEHOLDER.EMAIL}
-                        onChange={props.onChange}
-                        invalid={emailInvalid}
-                        valid={emailValid}
-                        onBlur={onBlurEmail}
-                    />
-                    <FormFeedback>{RULE.EMAIL}</FormFeedback>
-                </FormGroup>
-                <FormGroup>
-                    <Label for='password'>{FIELD_LABEL.PASSWORD}</Label>
-                    <Input
-                        className={'signup-form__input'}
-                        type='password'
-                        name='password'
-                        id='password'
-                        placeholder={FIELD_PLACEHOLDER.PASSWORD}
-                        onChange={props.onChange}
-                        invalid={passwordInvalid}
-                        valid={passwordValid}
-                        onBlur={onBlurPassword}
-                    />
-                    <FormFeedback>{RULE.PASSWORD}</FormFeedback>
-                </FormGroup>
-                <FormGroup>
-                    <Label for='confirmPassword'>{FIELD_LABEL.CONFIRM_PASSWORD}</Label>
-                    <Input
-                        className={'signup-form__input'}
-                        name='confirmPassword'
-                        type='password'
-                        id='confirmPassword'
-                        placeholder={FIELD_PLACEHOLDER.CONFIRM_PASSWORD}
-                        onChange={props.onChange}
-                        invalid={confirmPasswordInvalid}
-                        valid={confirmPasswordValid}
-                        onBlur={onBlurConfirmPassword}
-                    />
-                    <FormFeedback>{RULE.CONFIRM_PASSWORD}</FormFeedback>
-                </FormGroup>
-                <FormGroup className={'button-container'}>
-                    <Button className={'signup-form__button'} onClick={props.onSignupClick}>{BUTTON.SIGN_UP}</Button>
-                </FormGroup>
-            </Form>
-        </Container>
+        <div className={'signup-form__container'}>
+            <Container fluid={true}>
+                <Form>
+                    <FormGroup>
+                        <Label for='name'>{FIELD_LABEL.NAME}</Label>
+                        <Input
+                            className={'signup-form__input'}
+                            name='name'
+                            id='name'
+                            placeholder={FIELD_PLACEHOLDER.NAME}
+                            onChange={props.onChange}
+                            invalid={nameInvalid}
+                            valid={nameValid}
+                            onBlur={onBlurName}
+                        />
+                        <FormFeedback>{RULE.NAME}</FormFeedback>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for='email'>{FIELD_LABEL.EMAIL}</Label>
+                        <Input
+                            className={'signup-form__input'}
+                            name='email'
+                            id='email'
+                            placeholder={FIELD_PLACEHOLDER.EMAIL}
+                            onChange={props.onChange}
+                            invalid={emailInvalid}
+                            valid={emailValid}
+                            onBlur={onBlurEmail}
+                        />
+                        <FormFeedback>{RULE.EMAIL}</FormFeedback>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for='password'>{FIELD_LABEL.PASSWORD}</Label>
+                        <Input
+                            className={'signup-form__input'}
+                            type='password'
+                            name='password'
+                            id='password'
+                            placeholder={FIELD_PLACEHOLDER.PASSWORD}
+                            onChange={props.onChange}
+                            invalid={passwordInvalid}
+                            valid={passwordValid}
+                            onBlur={onBlurPassword}
+                        />
+                        <FormFeedback>{RULE.PASSWORD}</FormFeedback>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for='confirmPassword'>{FIELD_LABEL.CONFIRM_PASSWORD}</Label>
+                        <Input
+                            className={'signup-form__input'}
+                            name='confirmPassword'
+                            type='password'
+                            id='confirmPassword'
+                            placeholder={FIELD_PLACEHOLDER.CONFIRM_PASSWORD}
+                            onChange={props.onChange}
+                            invalid={confirmPasswordInvalid}
+                            valid={confirmPasswordValid}
+                            onBlur={onBlurConfirmPassword}
+                        />
+                        <FormFeedback>{RULE.CONFIRM_PASSWORD}</FormFeedback>
+                    </FormGroup>
+                    <FormGroup className={'button-container'}>
+                        <Button
+                            className={'signup-form__button'}
+                            onClick={props.onSignupClick}>{BUTTON.SIGN_UP}
+                        </Button>
+                    </FormGroup>
+                </Form>
+            </Container>
+        </div>
     );
 }
 
